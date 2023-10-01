f397978de734c4501321b7e9218fc8b9362bd7e9
Y-kishino <kishino0815@gmail.com>
2023-10-01 23:27:32 +0900

    Ver0.0.3

---
+++ components/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function footer(){
+    <React.Fragment>
+        <div>
+            <h2>
+                これはフッターです。
+            </h2>
+        </div>
+    </React.Fragment>
+}
